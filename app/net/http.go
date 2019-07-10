52937e7d58af268fbc030a49066d30549013e63f
Oleg Nesterov <olnester@redhat.com>
2019-07-10 18:15:32 +0300

    net listening

---
+++ app/net/http.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Main page")
+	w.Write([]byte("!!!"))
+	fmt.Println("Hi")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/page",
+		func(writer http.ResponseWriter, request *http.Request) {
+			fmt.Fprintln(writer, "Single page:", request.URL.String())
+		})
+
+	http.HandleFunc("pages",
+		func(writer http.ResponseWriter, request *http.Request) {
+			fmt.Fprintln(writer, "Multiple pages:", request.URL.String())
+		})
+
+	http.HandleFunc("/", handler)
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
+
+}
